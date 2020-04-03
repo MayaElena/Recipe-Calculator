68dd3ef820626cdbba6a94f3d2f630a508929da2
mayaelena@email.arizona.edu <mayaelena@email.arizona.edu>
2020-04-03 14:02:06 -0700

    Initial Framework Commit, split off original repository

---
+++ assets/css/script.js
@@ -0,0 +1,61 @@
+// BOILERPLATE
+$(document).ready(function () {
+    // BOILERPLATE
+
+    // Begin space for javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // End Space for javascript
+    // BOILERPLATE
+});
+// BOILERPLATE
---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+// BOILERPLATE
+$(document).ready(function () {
+    // BOILERPLATE
+
+    // Begin space for javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // End Space for javascript
+    // BOILERPLATE
+});
+// BOILERPLATE
