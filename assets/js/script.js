ef3b7d2747fd264af965ac0d173b325cdfeaecd8
mayaelena@email.arizona.edu <mayaelena@email.arizona.edu>
2020-04-14 15:14:50 -0700

    Transition to computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,46 @@
 // BOILERPLATE
 $(document).ready(function () {
+    // API KEY
+    var API_KEY = 'OvxhffEpFz6pKGUuHIfZW9MW8ZxJ2QcSmCtP1j1V';
+
     // BOILERPLATE
 
     // Begin space for javascript
+    console.log ('calculator Time');
+
+    var searchTerm = "cheddar cheese";
+
+    $.ajax({
+        method:"GET",
+        url: "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=" + API_KEY + "&query=" + searchTerm + "&dataType=Foundation"
+    }).then(function(res){
+        console.log(res);
+    });
 
 
 
 
 
+    // Folate mcgs
 
 
 
 
+    var ingredients = [
+        {
+            name: "Bell Pepper",
+
+        }
+    ];
 
 
 
+    /**
+     * This function takes in 
+     */
+    function calculateInformation(){
 
-
-
-
-
-
-
-
-
+    }
 
 
 
